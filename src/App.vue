61dc97476a4f82ed156e12881f986f1cb5454e89
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-11-17 22:58:20 -0800

    create componets folder create components register components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <compHeader/>
+    <p>{{name}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import compHeader from './Components/Header_Footer/Header';
 export default {
-  name: 'App',
+  data(){
+    return{
+      name: 'jalal',
+    }
+  },
   components: {
-    HelloWorld
+    compHeader,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
