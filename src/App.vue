63151235485e8eab6acfdc08f02f844f4e1f4c43
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-11-22 22:30:14 -0800

    integrate boostrap with vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div>
     <ListHelper>
-      <ul slot="mylist">
-        <li v-for="li in list" :key="li">{{ li }}</li>
-      </ul>
-      <p slot="para">User also knows php</p>
-      <p :slot="loader">See you again</p>
-    </ListHelper>
-    <compHeader/>
-    <comp-footer/>
-    <child-header :myName="name1"></child-header>
-    <child-header :my-student="students"></child-header>
-    <parent-footer
-        :firstname="fname"
-        :lastname="lname"
-        :age="myAge"
-        @updatevalue = "fname = $event"
-    ></parent-footer>
 
-    <p>{{name}}</p>
+    </ListHelper>
+<!--    <compHeader/>-->
+<!--    <comp-footer/>-->
+<!--    <child-header :myName="name1"></child-header>-->
+<!--    <child-header :my-student="students"></child-header>-->
+<!--    <parent-footer-->
+<!--        :firstname="fname"-->
+<!--        :lastname="lname"-->
+<!--        :age="myAge"-->
+<!--        @updatevalue = "fname = $event"-->
+<!--    ></parent-footer>-->
+
+<!--    <p>{{name}}</p>-->
   </div>
 </template>
 
 <script>
-import compHeader from './Components/Header_Footer/Header';
-import compFooter from './Components/Header_Footer/Footer';
-import ChildHeader from './Components/Parent_Control_Chil/Header';
-import ParentFooter from './Components/Child_Control_Parent/Footer';
+// import compHeader from './Components/Header_Footer/Header';
+// import compFooter from './Components/Header_Footer/Footer';
+// import ChildHeader from './Components/Parent_Control_Chil/Header';
+// import ParentFooter from './Components/Child_Control_Parent/Footer';
 import ListHelper from './Components/LisHelper';
 export default {
   data(){
@@ -42,10 +38,10 @@ export default {
     }
   },
   components: {
-    compHeader,
-    compFooter,
-    ChildHeader,
-    ParentFooter,
+    // compHeader,
+    // compFooter,
+    // ChildHeader,
+    // ParentFooter,
     ListHelper
   },
   created() {
