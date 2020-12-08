a55962ffdc8eea9f0a71dcdd35667a2bc3e3e3d8
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-12-07 21:44:12 -0800

    daynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,92 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <ListHelper>-->
+
+<!--    </ListHelper>-->
+<!--&lt;!&ndash;    <compHeader/>&ndash;&gt;-->
+<!--&lt;!&ndash;    <comp-footer/>&ndash;&gt;-->
+<!--&lt;!&ndash;    <child-header :myName="name1"></child-header>&ndash;&gt;-->
+<!--&lt;!&ndash;    <child-header :my-student="students"></child-header>&ndash;&gt;-->
+<!--&lt;!&ndash;    <parent-footer&ndash;&gt;-->
+<!--&lt;!&ndash;        :firstname="fname"&ndash;&gt;-->
+<!--&lt;!&ndash;        :lastname="lname"&ndash;&gt;-->
+<!--&lt;!&ndash;        :age="myAge"&ndash;&gt;-->
+<!--&lt;!&ndash;        @updatevalue = "fname = $event"&ndash;&gt;-->
+<!--&lt;!&ndash;    ></parent-footer>&ndash;&gt;-->
+
+<!--&lt;!&ndash;    <p>{{name}}</p>&ndash;&gt;-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--// import compHeader from './Components/Header_Footer/Header';-->
+<!--// import compFooter from './Components/Header_Footer/Footer';-->
+<!--// import ChildHeader from './Components/Parent_Control_Chil/Header';-->
+<!--// import ParentFooter from './Components/Child_Control_Parent/Footer';-->
+<!--import ListHelper from './Components/LisHelper';-->
+<!--export default {-->
+<!--  data(){-->
+<!--    return{-->
+<!--      name: 'jalal',-->
+<!--      name1: 'khan',-->
+<!--      students:['jalal','jehad','rume','arman'],-->
+<!--      fname:'uzzal',-->
+<!--      lname: 'khan',-->
+<!--      myAge: 21,-->
+<!--      list:['Html','css','java script'],-->
+<!--      loader: ''-->
+<!--    }-->
+<!--  },-->
+<!--  components: {-->
+<!--    // compHeader,-->
+<!--    // compFooter,-->
+<!--    // ChildHeader,-->
+<!--    // ParentFooter,-->
+<!--    ListHelper-->
+<!--  },-->
+<!--  created() {-->
+<!--    setTimeout(()=>{-->
+<!--      this.loader = 'other';-->
+<!--    },3000)-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+<!--&lt;!&ndash;global style&ndash;&gt;-->
+<!--<style>-->
+<!--/*div{*/-->
+<!--/*  color: red;*/-->
+<!--/*}*/-->
+<!--body{-->
+<!--  font-family: 'Roboto', sans-serif;-->
+<!--}-->
+
+
+<!--</style>-->
 <template>
-  <div>
-    <ListHelper>
-
-    </ListHelper>
-<!--    <compHeader/>-->
-<!--    <comp-footer/>-->
-<!--    <child-header :myName="name1"></child-header>-->
-<!--    <child-header :my-student="students"></child-header>-->
-<!--    <parent-footer-->
-<!--        :firstname="fname"-->
-<!--        :lastname="lname"-->
-<!--        :age="myAge"-->
-<!--        @updatevalue = "fname = $event"-->
-<!--    ></parent-footer>-->
-
-<!--    <p>{{name}}</p>-->
+  <div class="container">
+    <button @click="componentToRender='CompHome'">Home</button>
+    <button @click="componentToRender='CompContact'">Contact</button>
+    <button @click="componentToRender='CompAbout'">About</button>
+    <keep-alive>
+      <component :is="componentToRender"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-// import compHeader from './Components/Header_Footer/Header';
-// import compFooter from './Components/Header_Footer/Footer';
-// import ChildHeader from './Components/Parent_Control_Chil/Header';
-// import ParentFooter from './Components/Child_Control_Parent/Footer';
-import ListHelper from './Components/LisHelper';
+import CompHome from './Components/Home';
+import CompAbout from './Components/About';
+import CompContact from './Components/Contact';
 export default {
   data(){
     return{
-      name: 'jalal',
-      name1: 'khan',
-      students:['jalal','jehad','rume','arman'],
-      fname:'uzzal',
-      lname: 'khan',
-      myAge: 21,
-      list:['Html','css','java script'],
-      loader: ''
+      componentToRender: 'CompHome'
     }
   },
-  components: {
-    // compHeader,
-    // compFooter,
-    // ChildHeader,
-    // ParentFooter,
-    ListHelper
-  },
-  created() {
-    setTimeout(()=>{
-      this.loader = 'other';
-    },3000)
+  components:{
+    CompHome,
+    CompAbout,
+    CompContact
   }
 }
 </script>
-<!--global style-->
-<style>
-/*div{*/
-/*  color: red;*/
-/*}*/
-body{
-  font-family: 'Roboto', sans-serif;
-}
-
-
-</style>
