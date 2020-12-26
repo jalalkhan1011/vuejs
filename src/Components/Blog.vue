e97c911951eb8d66b6f8bdc0d8c941c646c4142d
Your Name <you@example.com>
2020-12-25 19:22:27 -0800

    get request

---
+++ src/Components/Blog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Blog Article</h1>
+    <div v-for="(blog,id) in blogs" :key="id">
+      <h1>{{ blog.title }}</h1>
+      <p>{{ blog.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      blogs:[]
+    }
+  },
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
+      console.log(data);
+      this.blogs = data.body.slice(0,10)
+    })
+  }
+}
+</script>
