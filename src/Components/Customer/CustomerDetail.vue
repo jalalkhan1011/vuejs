132174ceb73b95474b163be6d574fcab218dc69c
jalalkhan1011 <jalal.khan981@gmail.com>
2021-01-01 19:51:25 -0800

    query prameter

--- src/Components/Customer/CustomerDetail.vue
+++ src/Components/Customer/CustomerDetail.vue
@@ -2,7 +2,9 @@
   <div>
     <h2>Customer detail {{ $route.params.id }}</h2>
 
-    <router-link tag="button" :to="{ name:'customerEdit', params:'{id:$route.params.id}' }"
+<!--    <router-link tag="button" :to="{ name:'customerEdit', params:{id:$route.params.id} }"-->
+<!--                 class="btn btn-primary">Edit profile</router-link>-->
+    <router-link tag="button" :to="{ name:'customerEdit', params:{id:$route.params.id}, query:{local:'en',q:100}}"
                  class="btn btn-primary">Edit profile</router-link>
     <!--                 :to="'/customer/'+$route.params.id+'/edit'"-->
   </div>
--- src/Components/Customer/CustomerEdit.vue
+++ src/Components/Customer/CustomerEdit.vue
@@ -1,3 +1,7 @@
 <template>
-  <h2>Customer edit</h2>
+  <div>
+    <h2>Customer edit</h2>
+    <p>Locale: {{ $route.query.local }}</p>
+    <p>Analytics: {{ $route.query.q }}</p>
+  </div>
 </template>
